a868e1a07f3b2f57e032bcbcb7c9f7d807be515f
jgrden <jgrden@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2006-02-15 15:50:09 +0000

    Updated the batch file to check the value of JAVA_HOME rather than the existance of the variable.  Also updated it with a :finaly block.
    git-svn-id: http://red5.googlecode.com/svn/java/server/trunk@301 1b6495e4-3631-0410-8e05-8f51eee8b9cc

--- red5.bat
+++ red5.bat
@@ -1,11 +1,14 @@
 @echo off
 
-if not defined JAVA_HOME goto err
+if not %JAVA_HOME% == "" goto launchRed5
 
+:launchRed5
 %JAVA_HOME%/bin/java -jar red5.jar
+goto finaly
 
 :err
-
 echo JAVA_HOME environment variable not set! Take a look at the readme.
+pause
 
+:finaly
 pause
