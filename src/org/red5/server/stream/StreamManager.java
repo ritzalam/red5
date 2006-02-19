31425c3bf1446972ae1c8827c28bf1ac1e73e7ce
lhubbard <lhubbard@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2006-02-19 18:43:08 +0000

    Added append mode.
    git-svn-id: http://red5.googlecode.com/svn/java/server/trunk@359 1b6495e4-3631-0410-8e05-8f51eee8b9cc

--- src/org/red5/server/stream/StreamManager.java
+++ src/org/red5/server/stream/StreamManager.java
@@ -33,23 +33,33 @@ public class StreamManager implements ApplicationContextAware {
 	}
 
 	public void publishStream(Stream stream){
+		
+		// If we have a read mode stream, we shouldnt be publishing return
+		if(stream.getMode().equals(Stream.MODE_READ)) return;
+		
 		MultiStreamSink multi = new MultiStreamSink();
 		stream.setUpstream(multi);
 		published.put(stream.getName(),multi);
-		if(stream.getMode().equals(Stream.MODE_RECORD)){
-			try {				
-				Resource res = appCtx.getResource("streams/" + stream.getName()+".flv");
-				if(res.exists()) res.getFile().delete();
-				if(!res.exists()) res = appCtx.getResource("streams/").createRelative(stream.getName()+".flv");
-				if(!res.exists()) res.getFile().createNewFile(); 
-				File file = res.getFile();
-				FLV flv = flvService.getFLV(file);
-				Writer writer = flv.writer();
-				writer.writeHeader();
-				multi.connect(new FileStreamSink(writer));
-			} catch (IOException e) {
-				log.error("Error recording stream: "+stream, e);
-			}
+		
+		// If the mode is live, we dont need to do anything else
+		if(stream.getMode().equals(Stream.MODE_LIVE)) return;
+		
+		// The mode must be record or append
+		try {				
+			Resource res = appCtx.getResource("streams/" + stream.getName()+".flv");
+			if(res.exists()) res.getFile().delete();
+			if(!res.exists()) res = appCtx.getResource("streams/").createRelative(stream.getName()+".flv");
+			if(!res.exists()) res.getFile().createNewFile(); 
+			File file = res.getFile();
+			FLV flv = flvService.getFLV(file);
+			Writer writer = null; 
+			if(stream.getMode().equals(Stream.MODE_RECORD)) 
+				writer = flv.writer();
+			else if(stream.getMode().equals(Stream.MODE_APPEND))
+				writer = flv.append();
+			multi.connect(new FileStreamSink(writer));
+		} catch (IOException e) {
+			log.error("Error recording stream: "+stream, e);
 		}
 	}
 	
