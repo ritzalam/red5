758552001c69c83d846284bab452c11beb1026ec
jbauch <jbauch@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2006-03-14 10:32:56 +0000

    workaround for duplicate usage of same video codec instance for different streams
    git-svn-id: http://red5.googlecode.com/svn/java/server/trunk@523 1b6495e4-3631-0410-8e05-8f51eee8b9cc

--- src/org/red5/server/stream/VideoCodecFactory.java
+++ src/org/red5/server/stream/VideoCodecFactory.java
@@ -22,7 +22,17 @@ public class VideoCodecFactory {
 		IVideoStreamCodec result = null;
 		Iterator it = this.codecs.iterator();
 		while (it.hasNext()) {
-			IVideoStreamCodec codec = (IVideoStreamCodec) it.next();
+			IVideoStreamCodec codec;
+			IVideoStreamCodec storedCodec = (IVideoStreamCodec) it.next();
+			// XXX: this is a bit of a hack to create new instances of the configured
+			//      video codec for each stream
+			try {
+				codec = (IVideoStreamCodec) storedCodec.getClass().newInstance();
+			} catch (Exception e) {
+				log.error("Could not create video codec instance.", e);
+				continue;
+			}
+			
 			log.info("Trying codec " + codec);
 			if (codec.canHandleData(data)) {
 				result = codec;
