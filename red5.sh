a203673a4e1288f35b1b09643f59df5167db3091
seisenmann <seisenmann@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2006-03-21 16:38:35 +0000

    added java binary fallback added bash interpreter definition replace bash process with java process on start
    git-svn-id: http://red5.googlecode.com/svn/java/server/trunk@576 1b6495e4-3631-0410-8e05-8f51eee8b9cc

--- red5.sh
+++ red5.sh
@@ -1,1 +1,11 @@
-$JAVA_HOME/bin/java -cp red5.jar:$CLASSPATH org.red5.server.Standalone
+#!/bin/bash
+
+for JAVA in "$JAVA_HOME/bin/java" "/usr/bin/java" "/usr/local/bin/java"
+do
+  if [ -x $JAVA ]
+  then
+    break
+  fi
+done
+
+exec $JAVA -cp red5.jar:$CLASSPATH org.red5.server.Standalone
