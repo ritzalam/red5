1347385fc19154d085a2840513a0ead2f0ce73cc
mondain <mondain@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2006-04-08 21:11:39 +0000

    git-svn-id: http://red5.googlecode.com/svn/java/server/trunk@658 1b6495e4-3631-0410-8e05-8f51eee8b9cc

---
+++ src/org/red5/server/scheduling/ExampleBusinessObject.java
@@ -0,0 +1,52 @@
+package org.red5.server.scheduling;
+
+/*
+ * RED5 Open Source Flash Server - http://www.osflash.org/red5
+ * 
+ * Copyright © 2006 by respective authors. All rights reserved.
+ * 
+ * This library is free software; you can redistribute it and/or modify it under the 
+ * terms of the GNU Lesser General Public License as published by the Free Software 
+ * Foundation; either version 2.1 of the License, or (at your option) any later 
+ * version. 
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along 
+ * with this library; if not, write to the Free Software Foundation, Inc., 
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ * 
+ * @author The Red5 Project (red5@osflash.org)
+ * @author Dominick Accattato (daccattato@gmail.com)
+ * @author Luke Hubbard, Codegent Ltd (luke@codegent.com)
+ */
+
+/**
+ * ExampleBusinessObject
+ * This will be replaced by an actual service.  Just use this as 
+ * a template while implementing scheduled tasks
+ * 
+ * @author The Red5 Project (red5@osflash.org)
+ * @author Dominick Accattato (Dominick@gmail.com)
+ * @version 0.4
+ */
+
+public class ExampleBusinessObject {
+	private int counter = 0;
+	
+	public ExampleBusinessObject() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	/**
+	 * This is the method that get called on a specific schedule
+	 * @return void
+	 */
+	public void doSomething() {
+		//System.out.println("timer: " + counter++);
+	}
+
+}
