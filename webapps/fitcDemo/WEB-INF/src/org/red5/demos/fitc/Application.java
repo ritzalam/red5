e02f4e4b1156c08f49e2fe4ca3b46e42b92de0b2
jbauch <jbauch@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2006-04-15 20:21:58 +0000

    the application now keeps a list of connected streams, get it using a call to "getStreams"
    git-svn-id: http://red5.googlecode.com/svn/java/server/trunk@716 1b6495e4-3631-0410-8e05-8f51eee8b9cc

--- webapps/fitcDemo/WEB-INF/src/org/red5/demos/fitc/Application.java
+++ webapps/fitcDemo/WEB-INF/src/org/red5/demos/fitc/Application.java
@@ -1,6 +1,8 @@
 package org.red5.demos.fitc;
 
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.Set;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.red5.server.adapter.ApplicationAdapter;
@@ -9,6 +11,7 @@ import org.red5.server.api.IClient;
 import org.red5.server.api.IConnection;
 import org.red5.server.api.IScope;
 import org.red5.server.api.Red5;
+import org.red5.server.api.ScopeUtils;
 import org.red5.server.api.service.IPendingServiceCall;
 import org.red5.server.api.service.IPendingServiceCallback;
 import org.red5.server.api.service.IServiceCapableConnection;
@@ -19,21 +22,33 @@ public class Application extends ApplicationAdapter implements IPendingServiceCa
 	private static final Log log = LogFactory.getLog(Application.class);
 	
 	private final static String CLIENT_ID = "fitc.ClientId";
+	private final static String STREAMS_ID = "fitc.Streams";
 	
-	public boolean appStart(IScope scope) {
-		if (!super.appStart(scope))
+	public boolean start(IScope scope) {
+		if (!super.start(scope))
 			return false;
 		
-		// Initialize id generation
+		// Initialize id generation and list of streams
+		if (ScopeUtils.isApp(scope)) {
+			synchronized (scope) {
+				scope.setAttribute(CLIENT_ID, 0);
+			}
+		}
+		
+		// Every scope keeps a list of connected streams
 		synchronized (scope) {
-			scope.setAttribute(CLIENT_ID, 0);
+			scope.setAttribute(STREAMS_ID, new HashSet<String>());
 		}
 		return true;
 	}
 	
-	public boolean appJoin(IClient client, IScope app) {
-		if (!super.appJoin(client, app))
+	public boolean join(IClient client, IScope scope) {
+		if (!super.join(client, scope))
 			return false;
+		
+		// Only set the id for the application
+		if (!ScopeUtils.isApp(scope))
+			return true;
 		
 		Integer id;
 		if (!client.hasAttribute(CLIENT_ID)) {
@@ -55,6 +70,7 @@ public class Application extends ApplicationAdapter implements IPendingServiceCa
 		if (conn instanceof IServiceCapableConnection) {
 			IServiceCapableConnection service = (IServiceCapableConnection) conn;
 			
+			log.info("Setting id " + id + " for client " + client);
 			service.invoke("setId", new Object[]{id}, this);
 		}
 		
@@ -71,7 +87,8 @@ public class Application extends ApplicationAdapter implements IPendingServiceCa
 		
 		if (scope instanceof IBroadcastStream) {
 			IConnection current = Red5.getConnectionLocal();
-			Iterator<IConnection> it = scope.getParent().getConnections();
+			IScope parent = scope.getParent();
+			Iterator<IConnection> it = parent.getConnections();
 			while (it.hasNext()) {
 				IConnection conn = it.next();
 				if (conn.equals(current))
@@ -81,9 +98,32 @@ public class Application extends ApplicationAdapter implements IPendingServiceCa
 				if (conn instanceof IServiceCapableConnection)
 					((IServiceCapableConnection) conn).invoke("newStream", new Object[]{scope.getName()}, this);
 			}
+			
+			synchronized (parent) {
+				Set<String> streams = (Set<String>) parent.getAttribute(STREAMS_ID);
+				streams.add(scope.getName());
+			}
 		}
 		return true;
-		
 	}
 	
+	public void removeChildScope(IBasicScope scope) {
+		if (scope instanceof IBroadcastStream) {
+			IScope parent = scope.getParent();
+			synchronized (parent) {
+				Set<String> streams = (Set<String>) parent.getAttribute(STREAMS_ID);
+				streams.remove(scope.getName());
+			}
+		}
+		
+		super.removeChildScope(scope);
+	}
+	
+	synchronized public Set<String> getStreams() {
+		// Get list of streams from curent scope
+		IScope scope = Red5.getConnectionLocal().getScope();
+		log.info("Getting streams from " + scope);
+		Set<String> streams = (Set<String>) scope.getAttribute(STREAMS_ID);
+		return streams; 
+	}
 };
