98ff24114f015d2e112a4d20ba9efae24c008f69
jbauch <jbauch@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2006-05-30 22:42:24 +0000

    started hacking around with basic statistics service
    git-svn-id: http://red5.googlecode.com/svn/java/server/trunk@874 1b6495e4-3631-0410-8e05-8f51eee8b9cc

---
+++ src/org/red5/server/net/servlet/StatisticsServlet.java
@@ -0,0 +1,73 @@
+package org.red5.server.net.servlet;
+
+/*
+ * RED5 Open Source Flash Server - http://www.osflash.org/red5
+ * 
+ * Copyright © 2006 by respective authors (see below). All rights reserved.
+ * 
+ * This library is free software; you can redistribute it and/or modify it under the 
+ * terms of the GNU Lesser General Public License as published by the Free Software 
+ * Foundation; either version 2.1 of the License, or (at your option) any later 
+ * version. 
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along 
+ * with this library; if not, write to the Free Software Foundation, Inc., 
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ */
+
+import java.io.IOException;
+import java.io.OutputStream;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.xmlrpc.XmlRpcServer;
+import org.red5.server.api.IContext;
+import org.red5.server.statistics.ScopeStatistics;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+/**
+ * Servlet that processes the statistics XML-RPC requests.
+ * 
+ * @author Joachim Bauch (jojo@struktur.de)
+ *
+ */
+public class StatisticsServlet extends HttpServlet {
+
+	private static Log log =
+        LogFactory.getLog(StatisticsServlet.class.getName());
+	
+	private XmlRpcServer server = new XmlRpcServer();
+	protected WebApplicationContext webAppCtx;
+	protected IContext webContext;
+	
+	public void init() throws ServletException {
+		webAppCtx = WebApplicationContextUtils.getWebApplicationContext(getServletContext());
+		if (webAppCtx == null)
+			throw new ServletException("No web application context found.");
+		
+		webContext  = (IContext) webAppCtx.getBean("web.context");
+		
+		// Register handlers in XML-RPC server
+        server.addHandler("scopes", new ScopeStatistics(webContext));		
+	}
+	
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+    	throws ServletException, IOException {
+    	// Process request with XML-RPC server
+	    byte[] result = server.execute(request.getInputStream());
+	    response.setContentType("text/xml");
+	    response.setContentLength(result.length);
+	    OutputStream out = response.getOutputStream(); 
+	    out.write(result);
+	    out.close();
+	}
+}
---
+++ src/org/red5/server/statistics/ScopeStatistics.java
@@ -0,0 +1,120 @@
+package org.red5.server.statistics;
+
+/*
+ * RED5 Open Source Flash Server - http://www.osflash.org/red5
+ * 
+ * Copyright © 2006 by respective authors (see below). All rights reserved.
+ * 
+ * This library is free software; you can redistribute it and/or modify it under the 
+ * terms of the GNU Lesser General Public License as published by the Free Software 
+ * Foundation; either version 2.1 of the License, or (at your option) any later 
+ * version. 
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along 
+ * with this library; if not, write to the Free Software Foundation, Inc., 
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ */
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import org.red5.server.api.IContext;
+import org.red5.server.api.IScope;
+import org.red5.server.api.ScopeUtils;
+import org.red5.server.exception.ScopeNotFoundException;
+
+/**
+ * Public methods for XML-RPC scope statistics service.
+ * 
+ * @author Joachim Bauch (jojo@struktur.de)
+ *
+ */
+public class ScopeStatistics {
+
+	private IContext context;
+	
+	public ScopeStatistics(IContext context) {
+		this.context = context;
+	}
+	
+	/**
+	 * Resolve path to scope.
+	 * 
+	 * @param path
+	 * 			path to return scope for
+	 * @return the scope for the given path
+	 * @throws ScopeNotFoundException
+	 */
+	private IScope getScope(String path) throws ScopeNotFoundException {
+		IScope scope;
+		if (path != null && !path.equals("")) 
+			scope = ScopeUtils.resolveScope(context.getGlobalScope(), path);
+		else
+			scope = context.getGlobalScope();
+		
+		if (scope == null)
+			throw new ScopeNotFoundException(context.getGlobalScope(), path);
+		
+		return scope;
+	}
+	
+	/**
+	 * Return available applications. 
+	 * 
+	 * @return list of application names
+	 */
+	public String[] getScopes() {
+		return getScopes(null);
+	}
+	
+	/**
+	 * Return subscopes of another scope.
+	 * 
+	 * @param path
+	 * 			path of scope to return subscopes of
+	 * @return list of subscope names
+	 */
+	public String[] getScopes(String path) {
+		IScope scope = getScope(path);
+		List<String> result = new ArrayList<String>();
+		Iterator<String> iter = scope.getScopeNames();
+		while (iter.hasNext()) {
+			String name = iter.next();
+			result.add(name.substring(name.indexOf(IScope.SEPARATOR)+1));
+		}
+		
+		return (String[]) result.toArray(new String[result.size()]);
+	}
+	
+	/**
+	 * Return attributes of the global scope.
+	 * 
+	 * @return the scope's attributes
+	 */
+	public Map<String, Object> getScopeAttributes() {
+		return getScopeAttributes(null);
+	}
+	
+	/**
+	 * Return attributes of a given scope.
+	 * 
+	 * @param path
+	 * 			path of scope to return attributes of
+	 * @return the scope's attributes
+	 */
+	public Map<String, Object> getScopeAttributes(String path) {
+		IScope scope = getScope(path);
+		Map<String, Object> result = new Hashtable<String, Object>();
+		for (String name : scope.getAttributeNames()) {
+			// TODO: filter objects that can not be sent through XML-RPC
+			result.put(name, scope.getAttribute(name));
+		}
+		return result;
+	}
+}
