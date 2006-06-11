ee68a77fb9638cbccc979f2f18b2c46158726680
jbauch <jbauch@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2006-06-11 20:37:21 +0000

    moved method lookup to own class for easy reuse in other parts of Red5
    git-svn-id: http://red5.googlecode.com/svn/java/server/trunk@943 1b6495e4-3631-0410-8e05-8f51eee8b9cc

--- src/org/red5/server/service/ServiceInvoker.java
+++ src/org/red5/server/service/ServiceInvoker.java
@@ -25,14 +25,11 @@ package org.red5.server.service;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.red5.server.api.IContext;
 import org.red5.server.api.IScope;
 import org.red5.server.api.service.IPendingServiceCall;
 import org.red5.server.api.service.IServiceCall;
@@ -47,91 +44,6 @@ public class ServiceInvoker  implements IServiceInvoker {
 
 	public void setServiceResolvers(Set<IServiceResolver> resolvers) {
 		serviceResolvers = resolvers;
-	}
-	
-	/*
-	 * Returns (method, params) for the given service or (null, null) if not method was found.
-	 */
-	private Object[] findMethodWithExactParameters(Object service, String methodName, Object[] args)
-	{
-		int numParams = (args==null) ? 0 : args.length;
-		List methods = ConversionUtils.findMethodsByNameAndNumParams(service, methodName, numParams);
-		log.debug("Found " + methods.size() + " methods");
-		if (methods.isEmpty())
-			return new Object[]{null, null};
-		else if (methods.size() > 1) {
-			log.debug("Multiple methods found with same name and parameter count.");
-			log.debug("Parameter conversion will be attempted in order.");
-		}
-		
-		Method method = null;
-		Object[] params = null;
-		
-		// First search for method with exact parameters
-		for(int i=0; i<methods.size(); i++){
-			method = (Method) methods.get(i);
-			boolean valid = true;
-			Class[] paramTypes = method.getParameterTypes(); 
-			for (int j=0; j<args.length; j++) {
-				if (!args[j].getClass().equals(paramTypes[j])) {
-					valid = false;
-					break;
-				}
-			}
-			
-			if (valid)
-				return new Object[]{method, args};
-		}
-		
-		// Then try to convert parameters
-		for(int i=0; i<methods.size(); i++){
-			try {
-				method = (Method) methods.get(i);
-				params = ConversionUtils.convertParams(args, method.getParameterTypes());
-				return new Object[]{method, params};
-			} catch (Exception ex){
-				log.debug("Parameter conversion failed for " + method);
-			}
-		}
-		
-		return new Object[]{null, null};
-	}
-	
-	/*
-	 * Returns (method, params) for the given service or (null, null) if not method was found.
-	 */
-	private Object[] findMethodWithListParameters(Object service, String methodName, Object[] args)
-	{
-		List methods = ConversionUtils.findMethodsByNameAndNumParams(service, methodName, 1);
-		log.debug("Found " + methods.size() + " methods");
-		if (methods.isEmpty())
-			return new Object[]{null, null};
-		else if (methods.size() > 1) {
-			log.debug("Multiple methods found with same name and parameter count.");
-			log.debug("Parameter conversion will be attempted in order.");
-		}
-		
-		ArrayList argsList = new ArrayList();
-		if(args!=null){
-			for(int i=0; i<args.length; i++){
-				argsList.add(args[i]);
-			}
-		}
-		args = new Object[]{argsList};
-
-		Method method = null;
-		Object[] params = null;
-		for(int i=0; i<methods.size(); i++){
-			try {
-				method = (Method) methods.get(i);
-				params = ConversionUtils.convertParams(args, method.getParameterTypes());
-				return new Object[]{method, params};
-			} catch (Exception ex){
-				log.debug("Parameter conversion failed", ex);
-			}
-		}
-		
-		return new Object[]{null, null};
 	}
 	
 	/**
@@ -188,10 +100,9 @@ public class ServiceInvoker  implements IServiceInvoker {
 		}
 		
 		Object[] methodResult = null;
-		
-		methodResult = findMethodWithExactParameters(service, methodName, args);
+		methodResult = ServiceUtils.findMethodWithExactParameters(service, methodName, args);
 		if (methodResult.length == 0 || methodResult[0] == null)
-			methodResult = findMethodWithListParameters(service, methodName, args);
+			methodResult = ServiceUtils.findMethodWithListParameters(service, methodName, args);
 		
 		if (methodResult.length == 0 || methodResult[0] == null) {
 			log.error("Method " + methodName + " not found in " + service);
---
+++ src/org/red5/server/service/ServiceUtils.java
@@ -0,0 +1,121 @@
+package org.red5.server.service;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class ServiceUtils {
+
+	private static final Log log = LogFactory.getLog(ServiceUtils.class);
+
+	/**
+	 * Returns (method, params) for the given service or (null, null) if not method was found.
+	 */
+	public static Object[] findMethodWithExactParameters(Object service, String methodName, List args) {
+		Object[] arguments = new Object[]{args.size()};
+		for (int i=0; i<args.size(); i++) {
+			arguments[i] = args.get(i);
+		}
+		
+		return findMethodWithExactParameters(service, methodName, arguments);
+	}
+
+	/**
+	 * Returns (method, params) for the given service or (null, null) if not method was found.
+	 */
+	public static Object[] findMethodWithExactParameters(Object service, String methodName, Object[] args) {
+		int numParams = (args==null) ? 0 : args.length;
+		List methods = ConversionUtils.findMethodsByNameAndNumParams(service, methodName, numParams);
+		log.debug("Found " + methods.size() + " methods");
+		if (methods.isEmpty())
+			return new Object[]{null, null};
+		else if (methods.size() > 1) {
+			log.debug("Multiple methods found with same name and parameter count.");
+			log.debug("Parameter conversion will be attempted in order.");
+		}
+		
+		Method method = null;
+		Object[] params = null;
+		
+		// First search for method with exact parameters
+		for(int i=0; i<methods.size(); i++){
+			method = (Method) methods.get(i);
+			boolean valid = true;
+			Class[] paramTypes = method.getParameterTypes(); 
+			for (int j=0; j<args.length; j++) {
+				if (!args[j].getClass().equals(paramTypes[j])) {
+					valid = false;
+					break;
+				}
+			}
+			
+			if (valid)
+				return new Object[]{method, args};
+		}
+		
+		// Then try to convert parameters
+		for(int i=0; i<methods.size(); i++){
+			try {
+				method = (Method) methods.get(i);
+				params = ConversionUtils.convertParams(args, method.getParameterTypes());
+				return new Object[]{method, params};
+			} catch (Exception ex){
+				log.debug("Parameter conversion failed for " + method);
+			}
+		}
+		
+		return new Object[]{null, null};
+	}
+	
+	/**
+	 * Returns (method, params) for the given service or (null, null) if not method was found.
+	 */
+	public static Object[] findMethodWithListParameters(Object service, String methodName, List args) {
+		Object[] arguments = new Object[]{args.size()};
+		for (int i=0; i<args.size(); i++) {
+			arguments[i] = args.get(i);
+		}
+		
+		return findMethodWithListParameters(service, methodName, arguments);
+	}
+	
+	/**
+	 * Returns (method, params) for the given service or (null, null) if not method was found.
+	 */
+	public static Object[] findMethodWithListParameters(Object service, String methodName, Object[] args) {
+		List methods = ConversionUtils.findMethodsByNameAndNumParams(service, methodName, 1);
+		log.debug("Found " + methods.size() + " methods");
+		if (methods.isEmpty())
+			return new Object[]{null, null};
+		else if (methods.size() > 1) {
+			log.debug("Multiple methods found with same name and parameter count.");
+			log.debug("Parameter conversion will be attempted in order.");
+		}
+		
+		ArrayList argsList = new ArrayList();
+		if(args!=null){
+			for(int i=0; i<args.length; i++){
+				argsList.add(args[i]);
+			}
+		}
+		args = new Object[]{argsList};
+
+		Method method = null;
+		Object[] params = null;
+		for(int i=0; i<methods.size(); i++){
+			try {
+				method = (Method) methods.get(i);
+				params = ConversionUtils.convertParams(args, method.getParameterTypes());
+				return new Object[]{method, params};
+			} catch (Exception ex){
+				log.debug("Parameter conversion failed", ex);
+			}
+		}
+		
+		return new Object[]{null, null};
+	}
+	
+}
