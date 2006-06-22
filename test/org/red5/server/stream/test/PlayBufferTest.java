f9450f11238bce180f5883a7253db9afeb1abb3e
marijanm <marijanm@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2006-06-22 15:22:48 +0000

    first commit..testing 1.2.3..red5
    git-svn-id: http://red5.googlecode.com/svn/java/server/trunk@1005 1b6495e4-3631-0410-8e05-8f51eee8b9cc

---
+++ test/org/red5/server/stream/test/PlayBufferTest.java
@@ -0,0 +1,79 @@
+package org.red5.server.stream.test;
+/**
+ * @author m.j.milicevic <marijan at info.nl>
+ * @version 1.0
+ */
+
+import junit.framework.JUnit4TestAdapter;
+import junit.framework.Test;
+import junit.framework.TestCase;
+import org.apache.mina.common.ByteBuffer;
+import org.red5.server.net.rtmp.message.VideoData;
+import org.red5.server.stream.PlayBuffer;
+import org.red5.server.stream.message.RTMPMessage;
+
+/**
+ * TODO: extend testcase
+ */
+public class PlayBufferTest extends TestCase {
+	PlayBuffer playBuffer;
+	private RTMPMessage rtmpMessage;
+
+	protected void setUp() throws Exception {
+		super.setUp();
+		VideoData message = new VideoData();
+		message.setData(ByteBuffer.allocate(100));
+		playBuffer = new PlayBuffer(1000);
+		rtmpMessage = new RTMPMessage();
+		rtmpMessage.setBody(message);
+	}
+
+	public void testPlayBuffer() {
+		assertTrue("player buffer should be initialized", playBuffer != null);
+	}
+
+	public void testPutMessage() throws Exception {
+		enqueue();
+		RTMPMessage peek_message = playBuffer.peekMessage();
+		assertNotNull("message shouldn't be null", peek_message);
+		assertTrue(peek_message.equals(rtmpMessage));
+		dequeue();
+
+	}
+
+	public void testTakeMessage() throws Exception {
+		enqueue();
+		assertTrue(playBuffer.takeMessage().equals(rtmpMessage));
+		dequeue();
+	}
+
+	public void testPeekMessage() throws Exception {
+		enqueue();
+		assertTrue(playBuffer.peekMessage().equals(rtmpMessage));
+		dequeue();
+	}
+
+	public void testClear() {
+		enqueue();
+		playBuffer.clear();
+		assertTrue(playBuffer.getMessageCount() == 0);
+	}
+
+	public static Test suite() {
+		return new JUnit4TestAdapter(PlayBufferTest.class);
+	}
+
+
+	/**
+	 * enqueue with messages
+	 */
+	private void enqueue() {
+		boolean success = playBuffer.putMessage(rtmpMessage);
+		assertTrue("message successfully put into play buffer", success);
+	}
+
+	private void dequeue() throws Exception {
+		setUp();
+	}
+
+}
