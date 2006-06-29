73ed0799061e86066d62dffaabcccf7a4f4a01e2
jbauch <jbauch@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2006-06-29 13:14:15 +0000

    added code to determine frame type
    git-svn-id: http://red5.googlecode.com/svn/java/server/trunk@1072 1b6495e4-3631-0410-8e05-8f51eee8b9cc

--- src/org/red5/server/net/rtmp/event/VideoData.java
+++ src/org/red5/server/net/rtmp/event/VideoData.java
@@ -1,15 +1,38 @@
 package org.red5.server.net.rtmp.event;
 
 import org.apache.mina.common.ByteBuffer;
+import org.red5.io.IoConstants;
 import org.red5.server.stream.IStreamData;
 
-public class VideoData extends BaseEvent implements IStreamData {
+public class VideoData extends BaseEvent implements IoConstants, IStreamData {
 
+	public static enum FrameType {
+		UNKNOWN,
+		KEYFRAME,
+		INTERFRAME,
+		DISPOSABLE_INTERFRAME,
+	}
+	
 	protected ByteBuffer data = null;
+	private FrameType frameType = FrameType.UNKNOWN;
 	
 	public VideoData(ByteBuffer data){
 		super(Type.STREAM_DATA);
 		this.data = data;
+		if (data != null && data.limit() > 0) {
+			int oldPos = data.position();
+			int firstByte = ((int) data.get()) & 0xff;
+			data.position(oldPos);
+			int frameType = (firstByte & MASK_VIDEO_FRAMETYPE) >> 4;
+			if (frameType == FLAG_FRAMETYPE_KEYFRAME)
+				this.frameType = FrameType.KEYFRAME;
+			else if (frameType == FLAG_FRAMETYPE_INTERFRAME)
+				this.frameType = FrameType.INTERFRAME;
+			else if (frameType == FLAG_FRAMETYPE_DISPOSABLE)
+				this.frameType = FrameType.DISPOSABLE_INTERFRAME;
+			else
+				this.frameType = FrameType.UNKNOWN;
+		}
 	}
 
 	public byte getDataType() {
@@ -22,6 +45,10 @@ public class VideoData extends BaseEvent implements IStreamData {
 	
 	public String toString(){
 		return "Audio  ts: "+getTimestamp();
+	}
+	
+	public FrameType getFrameType() {
+		return frameType;
 	}
 	
 	public void release() {
