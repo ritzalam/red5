3895fd1773e01554458366b99d59d34b896702e2
jbauch <jbauch@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2006-07-25 10:24:05 +0000

    fixed synchronization issue when two clients are connecting to a to-be-created subscribe simultaneously
    git-svn-id: http://red5.googlecode.com/svn/java/server/trunk@1226 1b6495e4-3631-0410-8e05-8f51eee8b9cc

--- src/org/red5/server/ScopeResolver.java
+++ src/org/red5/server/ScopeResolver.java
@@ -47,11 +47,15 @@ public class ScopeResolver implements IScopeResolver {
 			if (room.equals(""))
 				// Skip empty path elements
 				continue;
-			if (scope.hasChildScope(room)){
-				scope = scope.getScope(room);
-			} else if (!scope.equals(globalScope) && scope.createChildScope(room)){
-				scope = scope.getScope(room);
-			} else throw new ScopeNotFoundException(scope,parts[i]);
+			
+			// Prevent the same subscope from getting created twice
+			synchronized (scope) {
+				if (scope.hasChildScope(room)){
+					scope = scope.getScope(room);
+				} else if (!scope.equals(globalScope) && scope.createChildScope(room)){
+					scope = scope.getScope(room);
+				} else throw new ScopeNotFoundException(scope,parts[i]);
+			}
 		}
 		return scope;
 	}
