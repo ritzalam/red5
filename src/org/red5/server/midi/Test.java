0c2f5aa403c1110fa1e3ea51ce11400198b752cb
lhubbard <lhubbard@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2006-08-26 02:36:23 +0000

    git-svn-id: http://red5.googlecode.com/svn/java/server/trunk@1280 1b6495e4-3631-0410-8e05-8f51eee8b9cc

---
+++ src/org/red5/server/midi/Test.java
@@ -0,0 +1,78 @@
+package org.red5.server.midi;
+
+import java.io.File;
+
+import javax.sound.midi.MidiDevice;
+import javax.sound.midi.MidiMessage;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Receiver;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class Test {
+
+	// Initialize Logging
+	protected static Log log =
+        LogFactory.getLog(Test.class.getName());
+	
+	public static void main(String[] args) throws Exception{
+		Test t = new Test();
+	}	
+	
+	public Test() throws Exception {
+		
+	
+		MidiPlayer player = new MidiPlayer(new File("/Users/luke/Desktop/Loops_Of_Fury.mid"));
+		
+		// Trying to get the events..
+		MidiSystem.getSequencer().getTransmitter().setReceiver(new MyReceiver());
+		MidiSystem.getTransmitter().setReceiver(new MyReceiver());
+				
+		MidiDevice.Info[] info =
+			MidiSystem.getMidiDeviceInfo();
+			
+			MidiDevice[] devices = new MidiDevice[info.length];
+			
+			try {
+				for (int i=0; i < info.length; i++) {
+					log.debug(info[i].getName());
+					MidiDevice device =
+						MidiSystem.getMidiDevice(info[i]);
+					log.debug(device);
+					//device.open();
+//				 Hook up a receiver to the transmitter
+					if(device.isOpen()){
+						device.getTransmitter().setReceiver(new MyReceiver());
+						devices[i] = device;
+					}
+				}
+			} catch (MidiUnavailableException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			// Wait long enough to play a few notes
+			// on the keyboard
+			Thread.sleep(30000);
+			
+			// Close the device (at program exit)
+			for(MidiDevice device : devices) if(device != null) device.close();
+			
+	}
+	
+	public class MyReceiver extends Object 
+		implements Receiver {
+		
+		public void send(MidiMessage msg,long time) {
+			System.err.println("dgg");
+			log.debug("Received message " + msg);
+		}
+	
+		public void close() {
+			log.debug("Closing");
+		}
+	}
+	
+}
