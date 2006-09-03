3072ef6024fb8de5592572cc2fd87ee793503450
lhubbard <lhubbard@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2006-09-03 19:31:25 +0000

    git-svn-id: http://red5.googlecode.com/svn/java/server/trunk@1308 1b6495e4-3631-0410-8e05-8f51eee8b9cc

--- webapps/midiDemo/WEB-INF/src/org/red5/demos/midi/Application.java
+++ webapps/midiDemo/WEB-INF/src/org/red5/demos/midi/Application.java
@@ -1,33 +1,121 @@
 package org.red5.demos.midi;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sound.midi.MidiDevice;
+import javax.sound.midi.MidiMessage;
+import javax.sound.midi.MidiSystem;
+import javax.sound.midi.MidiUnavailableException;
+import javax.sound.midi.Receiver;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.red5.server.adapter.ApplicationAdapter;
+import org.red5.server.api.IConnection;
 import org.red5.server.api.IScope;
-import org.red5.server.api.so.ISharedObject;
-import org.red5.server.midi.SharedMidiObject;
+import org.red5.server.api.Red5;
+import org.red5.server.api.service.IServiceCapableConnection;
 
 public class Application extends ApplicationAdapter {
 	
 	private static final Log log = LogFactory.getLog(Application.class);
 	
-	protected SharedMidiObject midiSo;
 	
 	@Override
 	public boolean appStart(IScope app) {
 		log.info("Midi demo app started");
-		// TODO Auto-generated method stub
 		return super.appStart(app);
 	}
+	
+	@Override
+	public void appDisconnect(IConnection conn) {
+		if(conn.hasAttribute("midi")){
+			MidiDevice dev = (MidiDevice) conn.getAttribute("midi");
+			if(dev.isOpen()) dev.close();
+		}
+		super.appDisconnect(conn);
+	}
 
-	public boolean connectMidiToSO(String deviceName, String soName){
-		log.info("Connecting midi device: "+deviceName+" to SO: "+soName);
-		ISharedObject so = getSharedObject(getScope(), soName, true);
-		midiSo = new SharedMidiObject(deviceName, so);
-		boolean success = midiSo.connect();
-		if(success) log.info("It worked!");
-		else log.error("Doh!");
-		return success;
+	public boolean connectToMidi(String deviceName){
+		IServiceCapableConnection conn = (IServiceCapableConnection) Red5.getConnectionLocal();
+		log.info("Connecting midi device: "+deviceName);
+		try {
+			MidiDevice dev = null;
+			// Close any existing device
+			if(conn.hasAttribute("midi")){
+				dev = (MidiDevice) conn.getAttribute("midi");
+				if(dev.isOpen()) dev.close();
+			}
+			// Lookup the current device
+			dev = getMidiDevice(deviceName);
+			if(dev == null) {
+				log.error("Midi device not found: "+deviceName);
+				return false;
+			}
+			// Open if needed
+			if(!dev.isOpen()) dev.open();
+			dev.getTransmitter().setReceiver(new MidiReceiver(conn));
+			log.info("It worked!");
+			// Save for later
+			conn.setAttribute("midi",dev);
+			return true;
+		} catch (MidiUnavailableException e) {
+			log.error("Error connecting to midi device", e);
+		} catch (RuntimeException e){
+			log.error("Error connecting to midi device", e);
+		}
+		log.error("Doh!");
+		return false;
+	}
+	
+	public static MidiDevice getMidiDevice(String name){
+		
+		MidiDevice.Info[] info = MidiSystem.getMidiDeviceInfo();
+		
+		for (int i = 0; i < info.length; i++) {
+			if(info[i].getName().equals(name)) {
+				try {
+					return MidiSystem.getMidiDevice(info[i]);
+				} catch (MidiUnavailableException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+		return null;
+		
+	}
+	
+	public class MidiReceiver extends Object implements Receiver {
+		
+		protected IServiceCapableConnection conn;
+		
+		public MidiReceiver(IServiceCapableConnection conn){
+			this.conn = conn;
+		}
+		
+		public void send(MidiMessage midi, long time) {
+			
+			byte[] msg = midi.getMessage();
+			int len = midi.getLength();
+			if(len <= 1) return; 
+			
+			conn.invoke("midi",new Object[]{time, msg});
+			
+			String out = "Midi >> Status: "+msg[0]+" Data: [";
+			for(int i=1; i<len; i++){
+				out += msg[i] + ((i==len-1) ? "" : ","); 
+			}
+			out += "]";
+			
+			log.debug(out);
+		}
+
+		public void close() {
+			log.debug("Midi device closed");
+		}
+
 	}
 	
 }
