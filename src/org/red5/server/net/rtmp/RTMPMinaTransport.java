a4060b8bea482bc5a7dc6ee5e4e74d10b7f8ad88
pgregoire <pgregoire@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2007-05-03 23:28:09 +0000

    added mbean for mina transport
    git-svn-id: http://red5.googlecode.com/svn/java/server/trunk@1950 1b6495e4-3631-0410-8e05-8f51eee8b9cc

--- src/org/red5/server/net/rtmp/RTMPMinaTransport.java
+++ src/org/red5/server/net/rtmp/RTMPMinaTransport.java
@@ -10,6 +10,11 @@ import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
+import javax.management.MBeanServer;
+import javax.management.ObjectName;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.mina.common.ByteBuffer;
 import org.apache.mina.common.IoFilter;
 import org.apache.mina.common.SimpleByteBufferAllocator;
@@ -19,57 +24,113 @@ import org.apache.mina.filter.executor.ExecutorFilter;
 import org.apache.mina.transport.socket.nio.SocketAcceptor;
 import org.apache.mina.transport.socket.nio.SocketAcceptorConfig;
 import org.apache.mina.transport.socket.nio.SocketSessionConfig;
+import org.red5.server.jmx.JMXFactory;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-
-/** 
+/**
  * Transport setup class configures socket acceptor and thread pools for RTMP in mina.
  * Note: This code originates from AsyncWeb, I've modified it for use with Red5. - Luke
  */
-public class RTMPMinaTransport {
+public class RTMPMinaTransport implements RTMPMinaTransportMBean {
 
 	private static final Log log = LogFactory.getLog(RTMPMinaTransport.class);
 
 	private static final int DEFAULT_PORT = 1935;
-	private static final int DEFAULT_IO_THREADS = Runtime.getRuntime().availableProcessors();
+
+	private static final int DEFAULT_IO_THREADS = Runtime.getRuntime()
+			.availableProcessors();
+
 	private static final int DEFAULT_EVENT_THREADS_CORE = 16;
+
 	private static final int DEFAULT_EVENT_THREADS_MAX = 32;
+
 	private static final int DEFAULT_EVENT_THREADS_QUEUE = -1;
+
 	private static final int DEFAULT_EVENT_THREADS_KEEPALIVE = 60;
+
 	private static final int DEFAULT_SEND_BUFFER_SIZE = 64 * 1024;
+
 	private static final int DEFAULT_RECEIVE_BUFFER_SIZE = 256 * 1024;
+
 	private static final boolean DEFAULT_TCP_NO_DELAY = false;
+
 	private static final boolean DEFAULT_USE_HEAP_BUFFERS = true;
 
 	private String address = null;
+
 	private int port = DEFAULT_PORT;
+
 	private int ioThreads = DEFAULT_IO_THREADS;
+
 	private int eventThreadsCore = DEFAULT_EVENT_THREADS_CORE;
+
 	private int eventThreadsMax = DEFAULT_EVENT_THREADS_MAX;
+
 	private int eventThreadsKeepalive = DEFAULT_EVENT_THREADS_KEEPALIVE;
+
 	private int eventThreadsQueue = DEFAULT_EVENT_THREADS_QUEUE;
+
 	private int sendBufferSize = DEFAULT_SEND_BUFFER_SIZE;
+
 	private int receiveBufferSize = DEFAULT_RECEIVE_BUFFER_SIZE;
+
 	private boolean tcpNoDelay = DEFAULT_TCP_NO_DELAY;
+
 	private boolean useHeapBuffers = DEFAULT_USE_HEAP_BUFFERS;
 
 	private SocketAcceptor acceptor;
+
 	private ExecutorService ioExecutor;
+
 	private ExecutorService eventExecutor;
+
 	private RTMPMinaIoHandler ioHandler;
 
 	private boolean isLoggingTraffic = false;
+
+	/**
+	 * MBean object name used for de/registration purposes.
+	 */
+	private ObjectName oName;
 
 	public void setAddress(String address) {
 		if ("*".equals(address) || "0.0.0.0".equals(address)) {
 			address = null;
 		}
 		this.address = address;
+		//update the mbean
+		if (null != oName) {
+			try {
+				// update the attribute
+				MBeanServer mbs = JMXFactory.getMBeanServer();
+				if (mbs.isRegistered(oName)) {
+					//TODO: get the correct address for fallback when address is null
+					mbs
+							.setAttribute(oName,
+									new javax.management.Attribute("address",
+											(address == null ? "0.0.0.0"
+													: address)));
+				}
+			} catch (Exception e) {
+				log.error("Exception updating mbean", e);
+			}
+		}
 	}
 
 	public void setPort(int port) {
 		this.port = port;
+		if (null != oName) {
+			//update the mbean
+			try {
+				// update the attribute
+				MBeanServer mbs = JMXFactory.getMBeanServer();
+				if (mbs.isRegistered(oName)) {
+					mbs.setAttribute(oName, new javax.management.Attribute(
+							"port", port));
+				}
+			} catch (Exception e) {
+				log.error("Exception updating mbean", e);
+			}
+		}
 	}
 
 	public void setIoThreads(int ioThreads) {
@@ -79,7 +140,7 @@ public class RTMPMinaTransport {
 	public void setEventThreadsCore(int eventThreadsCore) {
 		this.eventThreadsCore = eventThreadsCore;
 	}
-	
+
 	public void setEventThreadsMax(int eventThreadsMax) {
 		this.eventThreadsMax = eventThreadsMax;
 	}
@@ -87,11 +148,11 @@ public class RTMPMinaTransport {
 	public void setEventThreadsKeepalive(int eventThreadsKeepalive) {
 		this.eventThreadsKeepalive = eventThreadsKeepalive;
 	}
-	
+
 	public void setEventThreadsQueue(int eventThreadsQueue) {
 		this.eventThreadsQueue = eventThreadsQueue;
 	}
-	
+
 	public void setIsLoggingTraffic(boolean isLoggingTraffic) {
 		this.isLoggingTraffic = isLoggingTraffic;
 	}
@@ -111,66 +172,70 @@ public class RTMPMinaTransport {
 	public void setTcpNoDelay(boolean tcpNoDelay) {
 		this.tcpNoDelay = tcpNoDelay;
 	}
-	
+
 	public void setUseHeapBuffers(boolean useHeapBuffers) {
 		this.useHeapBuffers = useHeapBuffers;
 	}
 
 	public void start() throws Exception {
 		initIOHandler();
-		
+
 		ByteBuffer.setUseDirectBuffers(!useHeapBuffers); // this is global, oh well.
-		if(useHeapBuffers) ByteBuffer.setAllocator(new SimpleByteBufferAllocator()); // dont pool for heap buffers.
-		
+		if (useHeapBuffers)
+			ByteBuffer.setAllocator(new SimpleByteBufferAllocator()); // dont pool for heap buffers.
+
 		log.info("RTMP Mina Transport Settings");
-		log.info("IO Threads: "+ioThreads+"+1");
-		log.info("Event Threads:"
-				+" core: "+eventThreadsCore+"+1"
-				+" max: "+eventThreadsMax+"+1"
-				+" queue: "+eventThreadsQueue 
-				+" keepalive: "+eventThreadsKeepalive);
-		
-		ioExecutor = new ThreadPoolExecutor(
-				ioThreads + 1, ioThreads + 1, 
-				60, TimeUnit.SECONDS, 
-				new LinkedBlockingQueue<Runnable>());
-		
-		eventExecutor = new ThreadPoolExecutor(
-				eventThreadsCore + 1, eventThreadsMax + 1, 
-				eventThreadsKeepalive, TimeUnit.SECONDS, 
+		log.info("IO Threads: " + ioThreads + "+1");
+		log.info("Event Threads:" + " core: " + eventThreadsCore + "+1"
+				+ " max: " + eventThreadsMax + "+1" + " queue: "
+				+ eventThreadsQueue + " keepalive: " + eventThreadsKeepalive);
+
+		ioExecutor = new ThreadPoolExecutor(ioThreads + 1, ioThreads + 1, 60,
+				TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
+
+		eventExecutor = new ThreadPoolExecutor(eventThreadsCore + 1,
+				eventThreadsMax + 1, eventThreadsKeepalive, TimeUnit.SECONDS,
 				threadQueue(eventThreadsQueue));
-		
+
 		acceptor = new SocketAcceptor(ioThreads, ioExecutor);
-		
+
 		acceptor.getFilterChain().addLast("threadPool",
 				new ExecutorFilter(eventExecutor));
-		
+
 		SocketAcceptorConfig config = acceptor.getDefaultConfig();
 		config.setThreadModel(ThreadModel.MANUAL);
 		config.setReuseAddress(true);
 		config.setBacklog(100);
-		
-		log.info("TCP No Delay: "+tcpNoDelay);
-		log.info("Receive Buffer Size: "+ receiveBufferSize);
-		log.info("Send Buffer Size: "+sendBufferSize);
-		
-		SocketSessionConfig sessionConf = (SocketSessionConfig) config.getSessionConfig();
+
+		log.info("TCP No Delay: " + tcpNoDelay);
+		log.info("Receive Buffer Size: " + receiveBufferSize);
+		log.info("Send Buffer Size: " + sendBufferSize);
+
+		SocketSessionConfig sessionConf = (SocketSessionConfig) config
+				.getSessionConfig();
 		sessionConf.setReuseAddress(true);
 		sessionConf.setTcpNoDelay(tcpNoDelay);
 		sessionConf.setReceiveBufferSize(receiveBufferSize);
 		sessionConf.setSendBufferSize(sendBufferSize);
-		
+
 		if (isLoggingTraffic) {
 			log.info("Configuring traffic logging filter");
 			IoFilter filter = new LoggingFilter();
 			acceptor.getFilterChain().addFirst("LoggingFilter", filter);
 		}
-		
-		SocketAddress socketAddress = (address == null) ? new InetSocketAddress(port) : new InetSocketAddress(address, port);
+
+		SocketAddress socketAddress = (address == null) ? new InetSocketAddress(
+				port)
+				: new InetSocketAddress(address, port);
 		acceptor.bind(socketAddress, ioHandler);
 
 		log.info("RTMP Mina Transport bound to " + socketAddress.toString());
 
+		//create a new mbean for this instance
+		oName = JMXFactory.createMBean(
+				"org.red5.server.net.rtmp.RTMPMinaTransport", "address="
+						+ (address == null ? "0.0.0.0" : address) + ",port="
+						+ port);
 	}
 
 	public void stop() {
@@ -178,6 +243,13 @@ public class RTMPMinaTransport {
 		acceptor.unbindAll();
 		ioExecutor.shutdown();
 		eventExecutor.shutdown();
+
+		try {
+			// deregister with jmx
+			JMXFactory.getMBeanServer().unregisterMBean(oName);
+		} catch (Exception e) {
+			log.error("Exception unregistering mbean", e);
+		}
 	}
 
 	public String toString() {
@@ -190,15 +262,15 @@ public class RTMPMinaTransport {
 			ioHandler = new RTMPMinaIoHandler();
 		}
 	}
-	
-	private BlockingQueue<Runnable> threadQueue(int size){
-		switch(size){
-		case -1: 
-			return new LinkedBlockingQueue<Runnable>();
-		case 0:
-			return new SynchronousQueue<Runnable>();
-		default: 
-			return new ArrayBlockingQueue<Runnable>(size);	
+
+	private BlockingQueue<Runnable> threadQueue(int size) {
+		switch (size) {
+			case -1:
+				return new LinkedBlockingQueue<Runnable>();
+			case 0:
+				return new SynchronousQueue<Runnable>();
+			default:
+				return new ArrayBlockingQueue<Runnable>(size);
 		}
 	}
 }
---
+++ src/org/red5/server/net/rtmp/RTMPMinaTransportMBean.java
@@ -0,0 +1,25 @@
+package org.red5.server.net.rtmp;
+
+
+/** 
+ * 
+ */
+public interface RTMPMinaTransportMBean {
+
+	public void setAddress(String address);
+	public void setPort(int port);
+	public void setIoThreads(int ioThreads);
+	public void setEventThreadsCore(int eventThreadsCore);
+	public void setEventThreadsMax(int eventThreadsMax);
+	public void setEventThreadsKeepalive(int eventThreadsKeepalive);
+	public void setEventThreadsQueue(int eventThreadsQueue);
+	public void setIsLoggingTraffic(boolean isLoggingTraffic);
+	public void setIoHandler(RTMPMinaIoHandler rtmpIOHandler);
+	public void setReceiveBufferSize(int receiveBufferSize);
+	public void setSendBufferSize(int sendBufferSize);
+	public void setTcpNoDelay(boolean tcpNoDelay);
+	public void setUseHeapBuffers(boolean useHeapBuffers);
+	public void start() throws Exception;
+	public void stop();
+	
+}
