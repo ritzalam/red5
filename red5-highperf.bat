76cf4541e93900eba6bd2543f3374afb65e4f9da
pgregoire <pgregoire@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2007-05-09 16:17:33 +0000

    Added hi-performance startup batch file for windows
    git-svn-id: http://red5.googlecode.com/svn/java/server/trunk@1989 1b6495e4-3631-0410-8e05-8f51eee8b9cc

---
+++ red5-highperf.bat
@@ -0,0 +1,18 @@
+@echo off
+
+set PATH=.;c:\dev\java6\bin
+set CLASSPATH=.;C:\dev\QTJava.zip
+set JAVA_HOME=c:\dev\java6
+
+if not %JAVA_HOME% == "" goto launchRed5
+
+:launchRed5
+%JAVA_HOME%/bin/java -Djava.security.manager -Djava.security.policy=conf/red5.policy -Xrs -Xms512M -Xmx768M -Xss128K -XX:PermSize=256M -XX:MaxPermSize=512M -XX:NewRatio=2 -XX:MinHeapFreeRatio=20 -XX:+AggressiveHeap -XX:+DisableExplicitGC -XX:ParallelGCThreads=2 -XX:+UseParallelOldGC -XX:+MaxFDLimit -Dsun.rmi.dgc.client.gcInterval=990000 -Dsun.rmi.dgc.server.gcInterval=990000 -Djava.net.preferIPv4Stack=true -Xverify:none -cp red5.jar;conf;%CLASSPATH% org.red5.server.Standalone
+goto finaly
+
+:err
+echo JAVA_HOME environment variable not set! Take a look at the readme.
+pause
+
+:finaly
+pause
