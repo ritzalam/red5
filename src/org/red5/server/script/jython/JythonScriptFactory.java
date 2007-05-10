c85e85d6e3e452bcb31178b695d7f5194aed28e2
jbauch <jbauch@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2007-05-10 23:40:52 +0000

    Python scripts can import classes available only in the classpath of a webapp (Jira APPSERVER-92) updated to Jython 2.2b1
    git-svn-id: http://red5.googlecode.com/svn/java/server/trunk@2014 1b6495e4-3631-0410-8e05-8f51eee8b9cc

--- src/org/red5/server/script/jython/JythonScriptFactory.java
+++ src/org/red5/server/script/jython/JythonScriptFactory.java
@@ -1,11 +1,17 @@
 package org.red5.server.script.jython;
 
+import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
 
 import org.apache.log4j.Logger;
+import org.mortbay.jetty.handler.ContextHandler;
+import org.mortbay.jetty.webapp.WebAppContext;
+import org.python.core.Py;
 import org.python.core.PyFunction;
 import org.python.core.PyJavaInstance;
 import org.python.core.PyObject;
+import org.python.core.PySystemState;
 import org.python.util.PythonInterpreter;
 import org.springframework.scripting.ScriptCompilationException;
 import org.springframework.scripting.ScriptFactory;
@@ -72,11 +78,35 @@ public class JythonScriptFactory implements ScriptFactory {
 	/** {@inheritDoc} */
     public Object getScriptedObject(ScriptSource scriptSourceLocator, Class[] scriptInterfaces)
 			throws IOException, ScriptCompilationException {
+    	// TODO: how to do this when running under Tomcat?
+    	ContextHandler handler = WebAppContext.getCurrentWebAppContext();
+    	String basePath = "";
+    	if (handler != null) {
+    		File root = handler.getBaseResource().getFile();
+    		if (root != null && root.exists()) {
+    			basePath = root.getAbsolutePath() + File.separator + "WEB-INF" + File.separator;
+    		}
+    	}
+    	
 		String strScript = scriptSourceLocator.getScriptAsString();
-		
 		if (scriptInterfaces.length > 0) {   
 			try {
-				PythonInterpreter interp = new PythonInterpreter();
+				PySystemState state = new PySystemState();
+				if (!"".equals(basePath)) {
+					// Add webapp paths that can contain classes and .jar files to python search path
+					state.path.insert(0, Py.newString(basePath + "classes"));
+					File jarRoot = new File(basePath + "lib");
+					if (jarRoot.exists()) {
+						for (String filename: jarRoot.list(new FilenameFilter() {
+							public boolean accept(File dir, String name) {
+								return (name.endsWith(".jar"));
+							}
+						})) {
+							state.path.insert(1, Py.newString(basePath + "lib" + File.separator + filename));
+						}
+					}
+				}
+				PythonInterpreter interp = new PythonInterpreter(null, state);
 				interp.exec(strScript);
 				PyObject getInstance = interp.get("getInstance");
 				if (!(getInstance instanceof PyFunction)) {
