a46ddc210f723057a8561e22f53b489d5a724431
jbauch <jbauch@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2007-05-10 19:55:03 +0000

    added javadoc documentation
    git-svn-id: http://red5.googlecode.com/svn/java/server/trunk@2009 1b6495e4-3631-0410-8e05-8f51eee8b9cc

--- src/org/red5/io/amf3/IExternalizable.java
+++ src/org/red5/io/amf3/IExternalizable.java
@@ -19,10 +19,26 @@ package org.red5.io.amf3;
  * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
  */
 
+/**
+ * Interface that needs to be implemented by classes that serialize / deserialize
+ * themselves.
+ * 
+ * @see <a href="http://livedocs.adobe.com/flex/2/langref/flash/utils/IExternalizable.html">Adobe Livedocs (external)</a>
+ */
 public interface IExternalizable {
 
+	/**
+	 * Load custom object from stream.
+	 * 
+	 * @param input object to be used for data loading
+	 */
 	public void readExternal(IDataInput input);
 	
+	/**
+	 * Store custom object to stream.
+	 * 
+	 * @param output object to be used for data storing
+	 */
 	public void writeExternal(IDataOutput output);
 	
 }
