2c367014bbd5dbe2419630c074d0815bf355a319
jbauch <jbauch@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2007-06-11 21:50:05 +0000

    fixed handshake reply for Flash Player 9 Update 3
    git-svn-id: http://red5.googlecode.com/svn/java/server/trunk@2092 1b6495e4-3631-0410-8e05-8f51eee8b9cc

--- src/org/red5/server/net/rtmp/RTMPMinaIoHandler.java
+++ src/org/red5/server/net/rtmp/RTMPMinaIoHandler.java
@@ -141,7 +141,10 @@ implements ApplicationContextAware {
 			}
 			ByteBuffer out = ByteBuffer.allocate((Constants.HANDSHAKE_SIZE*2)+1);
 			out.put((byte)0x03);
-			out.fill((byte)0x00,Constants.HANDSHAKE_SIZE);
+			// TODO: the first four bytes of the handshake reply seem to be the
+			//       server uptime - send something better here...
+			out.putInt(0x01);
+			out.fill((byte)0x00,Constants.HANDSHAKE_SIZE-4);
 			out.put(in);
 			out.flip();
 			//in.release();
