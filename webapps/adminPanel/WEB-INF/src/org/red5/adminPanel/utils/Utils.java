ffb0ebb85a14115196852090930a25ade8795754
TTriemstra <TTriemstra@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2007-10-11 22:04:32 +0000

    Cosmetic.
    git-svn-id: http://red5.googlecode.com/svn/java/server/trunk@2387 1b6495e4-3631-0410-8e05-8f51eee8b9cc

--- webapps/adminPanel/WEB-INF/src/org/red5/adminPanel/utils/Utils.java
+++ webapps/adminPanel/WEB-INF/src/org/red5/adminPanel/utils/Utils.java
@@ -30,7 +30,7 @@ import java.util.Calendar;
  */
 public class Utils {
 	
-    public static String formatDate ( long d ){
+    public static String formatDate ( long d ) {
     	Calendar calendar = Calendar.getInstance();
 		calendar.setTimeInMillis( d );
 		SimpleDateFormat date = new SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss Z");
@@ -43,11 +43,11 @@ public class Utils {
     	
     	if (d < 1024)
     	{
-    		out = d + "b";
+    		out = d + " Bytes";
     	} else if (d > 1024) {
-    		out = (d / 1024) + "KB";
+    		out = (d / 1024) + " KB";
     	} else if (d > 104858) {
-    		out = (d / 1000000) + "MB";
+    		out = (d / 1000000) + " MB";
     	}
 		return out;
     }
