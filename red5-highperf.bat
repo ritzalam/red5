368b27e87475772274c791c95e37cf5760930188
pgregoire <pgregoire@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2007-12-19 01:20:06 +0000

    removed windows CLASSPATH var. updated to use new spring and mina libs.
    git-svn-id: http://red5.googlecode.com/svn/java/server/trunk@2496 1b6495e4-3631-0410-8e05-8f51eee8b9cc

--- red5-highperf.bat
+++ red5-highperf.bat
@@ -3,7 +3,7 @@
 if not "%JAVA_HOME%" == "" goto launchRed5
 
 :launchRed5
-"%JAVA_HOME%/bin/java" -Djava.security.manager -Djava.security.policy=conf/red5.policy -Xrs -Xms512M -Xmx768M -Xss128K -XX:PermSize=256M -XX:MaxPermSize=512M -XX:NewRatio=2 -XX:MinHeapFreeRatio=20 -XX:+AggressiveHeap -XX:+DisableExplicitGC -XX:ParallelGCThreads=2 -XX:+UseParallelOldGC -XX:+MaxFDLimit -Dsun.rmi.dgc.client.gcInterval=990000 -Dsun.rmi.dgc.server.gcInterval=990000 -Djava.net.preferIPv4Stack=true -Xverify:none -cp red5.jar;conf;%CLASSPATH% org.red5.server.Standalone
+"%JAVA_HOME%/bin/java" -Djava.security.manager -Djava.security.policy=conf/red5.policy -Xrs -Xms512M -Xmx768M -Xss128K -XX:PermSize=256M -XX:MaxPermSize=512M -XX:NewRatio=2 -XX:MinHeapFreeRatio=20 -XX:+AggressiveHeap -XX:+DisableExplicitGC -XX:ParallelGCThreads=2 -XX:+UseParallelOldGC -XX:+MaxFDLimit -Dsun.rmi.dgc.client.gcInterval=990000 -Dsun.rmi.dgc.server.gcInterval=990000 -Djava.net.preferIPv4Stack=true -Xverify:none -cp red5.jar;conf;bin org.red5.server.Standalone
 goto finaly
 
 :err
