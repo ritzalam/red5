363f7d47e154d89b3bf3ffbf083b84f559589b79
TTriemstra <TTriemstra@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2008-02-29 15:48:27 +0000

    Adding license headers.
    git-svn-id: http://red5.googlecode.com/svn/java/server/trunk@2695 1b6495e4-3631-0410-8e05-8f51eee8b9cc

--- src/org/red5/server/net/mrtmp/codec/MRTMPProtocolDecoder.java
+++ src/org/red5/server/net/mrtmp/codec/MRTMPProtocolDecoder.java
@@ -1,5 +1,24 @@
 package org.red5.server.net.mrtmp.codec;
 
+/*
+ * RED5 Open Source Flash Server - http://www.osflash.org/red5
+ * 
+ * Copyright (c) 2006-2008 by respective authors (see below). All rights reserved.
+ * 
+ * This library is free software; you can redistribute it and/or modify it under the 
+ * terms of the GNU Lesser General Public License as published by the Free Software 
+ * Foundation; either version 2.1 of the License, or (at your option) any later 
+ * version. 
+ * 
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY 
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A 
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along 
+ * with this library; if not, write to the Free Software Foundation, Inc., 
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
+ */
+
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -13,6 +32,9 @@ import org.apache.mina.filter.codec.ProtocolDecoderOutput;
 import org.red5.server.net.mrtmp.MRTMPPacket;
 import org.red5.server.net.rtmp.message.Packet;
 
+/**
+ * @author Steven Gong (steven.gong@gmail.com)
+ */
 public class MRTMPProtocolDecoder implements ProtocolDecoder {
 	private static final Logger log = LoggerFactory.getLogger(MRTMPProtocolDecoder.class);
 
