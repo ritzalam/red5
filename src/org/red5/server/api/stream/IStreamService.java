8460ca6068bf5e99e6b951fbbefdb7579c623dfa
pgregoire <pgregoire@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2008-05-16 20:15:42 +0000

    Added improvement from Victor JIRA SN-89
    git-svn-id: http://red5.googlecode.com/svn/java/server/trunk@2807 1b6495e4-3631-0410-8e05-8f51eee8b9cc

--- src/org/red5/server/api/stream/IStreamService.java
+++ src/org/red5/server/api/stream/IStreamService.java
@@ -1,5 +1,6 @@
 package org.red5.server.api.stream;
 
+import org.red5.server.api.IConnection;
 import org.red5.server.api.IScopeService;
 
 /*
--- src/org/red5/server/stream/StreamService.java
+++ src/org/red5/server/stream/StreamService.java
@@ -82,6 +82,65 @@ public class StreamService implements IStreamService {
 				.deleteStreamById(getCurrentStreamId());
 	}
 
+	/**
+	 * Close stream.
+	 * This method can close both IClientBroadcastStream (coming from Flash Player to Red5)
+	 * and ISubscriberStream (from Red5 to Flash Player).
+	 * Corresponding application handlers (streamSubscriberClose, etc.) are called as if
+	 * close was initiated by Flash Player.
+	 * 
+	 * It is recommended to remember stream id in application handlers, ex.:
+	 * <pre>
+	 * public void streamBroadcastStart(IBroadcastStream stream) {
+	 * 	super.streamBroadcastStart(stream);
+	 * 	if (stream instanceof IClientBroadcastStream) {
+	 * 		int publishedStreamId = ((ClientBroadcastStream)stream).getStreamId();
+	 * 		Red5.getConnectionLocal().setAttribute(PUBLISHED_STREAM_ID_ATTRIBUTE, publishedStreamId);
+	 * 	}
+	 * }
+	 * </pre>
+	 * <pre>
+	 * public void streamPlaylistItemPlay(IPlaylistSubscriberStream stream, IPlayItem item, boolean isLive) {
+	 * 	super.streamPlaylistItemPlay(stream, item, isLive);
+	 * 	Red5.getConnectionLocal().setAttribute(WATCHED_STREAM_ID_ATTRIBUTE, stream.getStreamId());
+	 * }
+	 * </pre>
+	 * When stream is closed, corresponding NetStream status will be sent to stream provider / consumers.
+	 * Implementation is based on Red5's StreamService.close()
+	 * @param connection client connection
+	 * @param streamId stream ID (number: 1,2,...)
+	 */
+	public static void closeStream(IConnection connection, int streamId) {
+		if (!(connection instanceof IStreamCapableConnection)) {
+			logger.warn("Connection is not instance of IStreamCapableConnection: {}", connection);
+			return;
+		}
+
+		IStreamCapableConnection scConnection = (IStreamCapableConnection) connection;
+		IClientStream stream = scConnection.getStreamById(streamId);
+		if (stream == null) {
+			logger.info("Stream not found: streamId={}, connection={}", streamId, connection);
+			return;
+		}
+
+		if (stream instanceof IClientBroadcastStream) {
+			// this is a broadcasting stream (from Flash Player to Red5)
+			IClientBroadcastStream bs = (IClientBroadcastStream) stream;
+			IBroadcastScope bsScope = (IBroadcastScope)connection.getScope().getBasicScope(
+					IBroadcastScope.TYPE, bs.getPublishedName());
+			if (bsScope != null && connection instanceof BaseConnection) {
+				((BaseConnection) connection).unregisterBasicScope(bsScope);
+			}
+		}
+		stream.close();
+		scConnection.deleteStreamById(streamId);
+
+		// in case of broadcasting stream, status is sent automatically by Red5
+		if (!(stream instanceof IClientBroadcastStream)) {
+			StreamService.sendNetStreamStatus(connection, StatusCodes.NS_PLAY_STOP, "Stream closed by server", stream.getName(), Status.STATUS, streamId);
+		}
+	}    
+    
 	/** {@inheritDoc} */
     public int createStream() {
 		IConnection conn = Red5.getConnectionLocal();
@@ -501,15 +560,44 @@ public class StreamService implements IStreamService {
      * @param streamId
      */
     private void sendNSFailed(RTMPConnection conn, String description, String name, int streamId) {
-		Status failed = new Status(StatusCodes.NS_FAILED);
-		failed.setClientid(streamId);
-		failed.setDesciption(description);
-		failed.setDetails(name);
-		failed.setLevel("error");
+    	StreamService.sendNetStreamStatus(conn, StatusCodes.NS_FAILED, description, name, Status.ERROR, streamId);
+    }    
+    
+	/**
+	 * Send <code>NetStream.Status</code> to client (Flash Player)
+	 * @param conn
+	 * @param statusCode see StatusCodes class
+	 * @param description
+	 * @param name
+	 * @param streamId
+	 */    
+    private void sendNSStatus(IConnection conn, String statusCode, String description, String name, int streamId) {
+    	StreamService.sendNetStreamStatus(conn, statusCode, description, name, Status.STATUS, streamId);
+	}
+    
+	/**
+	 * Send <code>NetStream.Status</code> to client (Flash Player)
+	 *  
+	 * @param conn
+	 * @param statusCode NetStream status code
+	 * @param description
+	 * @param name
+	 * @param status The status - error, warning, or status
+	 * @param streamId
+	 */
+	public static void sendNetStreamStatus(IConnection conn, String statusCode, String description, String name, String status, int streamId) {
+		if (!(conn instanceof RTMPConnection)) {
+			throw new RuntimeException("Connection is not RTMPConnection: " + conn);
+		}
 
-		// FIXME: there should be a direct way to send the status
-		Channel channel = conn.getChannel((byte) (4 + ((streamId-1) * 5)));
-		channel.sendStatus(failed);
-    }
+		Status s = new Status(statusCode);
+		s.setClientid(streamId);
+		s.setDesciption(description);
+		s.setDetails(name);
+		s.setLevel(status);
+
+		Channel channel = ((RTMPConnection) conn).getChannel((byte) (4 + ((streamId - 1) * 5)));
+		channel.sendStatus(s);
+	}    
     
 }
