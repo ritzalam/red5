2667fbd98bbbf5a6fe49260db59b599f7bab3bd2
pgregoire <pgregoire@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2008-08-14 05:59:26 +0000

    updated the bash startup scripts
    git-svn-id: http://red5.googlecode.com/svn/java/server/trunk@2916 1b6495e4-3631-0410-8e05-8f51eee8b9cc

--- red5-bootstrap.sh
+++ red5-bootstrap.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# JMX options
-JMX_OPTS=""
+# JAVA options
+JAVA_OPTS="-Dred5.root=/opt/red5"
 LOGBACK_OPTS="-Dlogback.ContextSelector=org.red5.logging.LoggingContextSelector -Dcatalina.useNaming=true"
 SECURITY_OPTS="-Djava.security.manager -Djava.security.debug=failure -Djava.security.policy=conf/red5.policy"
 # Jython options
 JYTHON="-Dpython.home=lib"
 
-for JAVA in "$JAVA_HOME/bin/java" "/usr/bin/java" "/usr/local/bin/java"
+for JAVA in "/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home/bin/java" "$JAVA_HOME/bin/java" "/usr/bin/java" "/usr/local/bin/java"
 do
   if [ -x $JAVA ]
   then
@@ -21,6 +21,8 @@ then
   exit
 fi
 
+#JAVA=/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home/bin/java
+
 # start Red5
 echo "Starting Red5..."
-exec $JAVA $JYTHON $JMX_OPTS $LOGBACK_OPTS $SECURITY_OPTS -cp red5.jar org.red5.server.Bootstrap 1>log\stdout.log 2>log\stderr.log
+exec $JAVA $JYTHON $JAVA_OPTS $LOGBACK_OPTS $SECURITY_OPTS -cp red5.jar org.red5.server.Bootstrap 1>log\stdout.log 2>log\stderr.log
--- red5.sh
+++ red5.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-# JMX options
-JMX_OPTS="-Djava.security.manager -Djava.security.policy=conf/red5.policy"
+# Java options
+JAVA_OPTS="-Dred5.root=/opt/red5 -Djava.security.manager -Djava.security.policy=conf/red5.policy"
 
 # Jython options
 JYTHON="-Dpython.home=lib"
 
-for JAVA in "$JAVA_HOME/bin/java" "/usr/bin/java" "/usr/local/bin/java"
+for JAVA in "/System/Library/Frameworks/JavaVM.framework/Versions/1.6/Home/bin/java" "$JAVA_HOME/bin/java" "/usr/bin/java" "/usr/local/bin/java"
 do
   if [ -x $JAVA ]
   then
@@ -22,4 +22,4 @@ fi
 
 # start Red5
 echo "Starting Red5..."
-exec $JAVA $JYTHON $JMX_OPTS -cp red5.jar:conf:$CLASSPATH org.red5.server.Standalone
+exec $JAVA $JYTHON $JAVA_OPTS -cp red5.jar:conf:$CLASSPATH org.red5.server.Standalone
