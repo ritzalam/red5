2e85bbeb8ea988504951b6d9138691727d8cc108
mondain <mondain@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2008-10-22 17:52:16 +0000

    fixed unit test to use junit 4.4 and springs updated abstract context test
    git-svn-id: http://red5.googlecode.com/svn/java/server/trunk@3277 1b6495e4-3631-0410-8e05-8f51eee8b9cc

--- test/org/red5/server/ScopeTest.java
+++ test/org/red5/server/ScopeTest.java
@@ -1,12 +1,14 @@
 package org.red5.server;
 
-import org.junit.Ignore;
+import junit.framework.TestCase;
+
 import org.junit.Test;
 import org.red5.server.api.IContext;
 import org.red5.server.api.IScope;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 
 /**
  * This is for testing Scope issues. First created to address:
@@ -14,7 +16,8 @@ import org.springframework.test.AbstractDependencyInjectionSpringContextTests;
  * 
  * @author Paul Gregoire (mondain@gmail.com)
  */
-public class ScopeTest extends AbstractDependencyInjectionSpringContextTests {
+@ContextConfiguration(locations={"ScopeTest.xml"})
+public class ScopeTest extends AbstractJUnit4SpringContextTests {
 
 	protected static Logger log = LoggerFactory.getLogger(ScopeTest.class);
 	
@@ -26,39 +29,22 @@ public class ScopeTest extends AbstractDependencyInjectionSpringContextTests {
 		System.setProperty("red5.config_root", "bin/conf");
 	}
 	
-	@Override
-	protected String[] getConfigLocations() {
-		return new String[] { "org/red5/server/ScopeTest.xml" };
+	{
+		log.debug("Property - user.dir: {}", System.getProperty("user.dir"));
+		log.debug("Property - red5.root: {}", System.getProperty("red5.root"));
+		log.debug("Property - red5.config_root: {}", System.getProperty("red5.config_root"));
 	}
-
-	@Override
-	protected void onSetUp() throws Exception {
-		System.out.println("onSetUp");
-		super.onSetUp();
-			
-		if (appScope == null) {
-			
-			log.debug("Property - user.dir: {}", System.getProperty("user.dir"));
-			log.debug("Property - red5.root: {}", System.getProperty("red5.root"));
-			log.debug("Property - red5.config_root: {}", System.getProperty("red5.config_root"));
-			
-			appScope = (WebScope) applicationContext.getBean("web.scope");
-			log.debug("Application / web scope: {}", appScope);
-		}
-	}
-
-	@Override
-	protected void onTearDown() throws Exception {
-		System.out.println("onTearDown");
-		super.onTearDown();
-	}	
 	
-	@Ignore
 	@Test
 	public void testScope() {
+
+		if (appScope == null) {
+    		appScope = (WebScope) applicationContext.getBean("web.scope");
+    		log.debug("Application / web scope: {}", appScope);
+		}
 		
 		//Room 1
-		assertTrue(appScope.createChildScope("room1"));
+		TestCase.assertTrue(appScope.createChildScope("room1"));
 		IScope room1 = appScope.getScope("room1");
 		log.debug("Room 1: {}", room1);
 		
@@ -66,7 +52,7 @@ public class ScopeTest extends AbstractDependencyInjectionSpringContextTests {
 		log.debug("Context 1: {}", rmCtx1);
 		
 		//Room 2
-		assertTrue(room1.createChildScope("room2"));
+		TestCase.assertTrue(room1.createChildScope("room2"));
 		IScope room2 = room1.getScope("room2");
 		log.debug("Room 2: {}", room2);
 		
@@ -74,7 +60,7 @@ public class ScopeTest extends AbstractDependencyInjectionSpringContextTests {
 		log.debug("Context 2: {}", rmCtx2);
 		
 		//Room 3
-		assertTrue(room2.createChildScope("room3"));
+		TestCase.assertTrue(room2.createChildScope("room3"));
 		IScope room3 = room2.getScope("room3");
 		log.debug("Room 3: {}", room3);
 		
@@ -82,7 +68,7 @@ public class ScopeTest extends AbstractDependencyInjectionSpringContextTests {
 		log.debug("Context 3: {}", rmCtx3);		
 		
 		//Room 4 attaches at Room 1 (per bug example)
-		assertTrue(room1.createChildScope("room4"));
+		TestCase.assertTrue(room1.createChildScope("room4"));
 		IScope room4 = room1.getScope("room4");
 		log.debug("Room 4: {}", room4);
 		
@@ -90,7 +76,7 @@ public class ScopeTest extends AbstractDependencyInjectionSpringContextTests {
 		log.debug("Context 4: {}", rmCtx4);		
 		
 		//Room 5
-		assertTrue(room4.createChildScope("room5"));
+		TestCase.assertTrue(room4.createChildScope("room5"));
 		IScope room5 = room4.getScope("room5");
 		log.debug("Room 5: {}", room5);
 		
@@ -106,8 +92,7 @@ public class ScopeTest extends AbstractDependencyInjectionSpringContextTests {
 		
 	}
 
-	private class DummyScope extends Scope {
-
-	}	
+	//private class DummyScope extends Scope {
+	//}	
 	
 }
