9106a92a4bb6c58e08562752328347202684f9b4
mondain <mondain@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2009-01-24 19:34:39 +0000

    put missing back slashes back in
    git-svn-id: http://red5.googlecode.com/svn/java/server/trunk@3417 1b6495e4-3631-0410-8e05-8f51eee8b9cc

--- red5-shutdown.bat
+++ red5-shutdown.bat
@@ -5,7 +5,7 @@ SETLOCAL
 if NOT DEFINED RED5_HOME for /F %%o IN ('cd') do set RED5_HOME=%%o
 
 set JAVA_OPTS=-Djavax.net.ssl.keyStore=%RED5_HOME%conf\keystore.jmx -Djavax.net.ssl.keyStorePassword=password
-set RED5_CLASSPATH=%RED5_HOME%red5.jar;%RED5_HOME%conf
+set RED5_CLASSPATH=%RED5_HOME%\red5.jar;%RED5_HOME%\conf
 set RED5_MAINCLASS=org.red5.server.Shutdown
 set RED5_OPTS=9999 red5user changeme
 
