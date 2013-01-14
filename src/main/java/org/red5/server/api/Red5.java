68e5907b884e2b8a82033b22adefcc10f96606cd
mondain@gmail.com <mondain@gmail.com@1b6495e4-3631-0410-8e05-8f51eee8b9cc>
2013-01-14 18:56:32 +0000

    Commit prior to release of 1.0.1
    git-svn-id: http://red5.googlecode.com/svn/java/server/trunk@4530 1b6495e4-3631-0410-8e05-8f51eee8b9cc

--- src/main/java/org/red5/server/api/Red5.java
+++ src/main/java/org/red5/server/api/Red5.java
@@ -44,7 +44,7 @@ import org.red5.server.api.scope.IScope;
  *   
  * @author The Red5 Project (red5@osflash.org)
  * @author Luke Hubbard (luke@codegent.com)
- * @author Paul Gregoire (mondain@gmail.com)  
+ * @author Paul Gregoire (mondain@gmail.com)
  */
 public final class Red5 {
 
@@ -69,7 +69,7 @@ public final class Red5 {
 	/**
 	 * Current server version for fmsVer requests 
 	 */
-	public static final String FMS_VERSION = "RED5/1,0,0,0";
+	public static final String FMS_VERSION = "RED5/1,0,1,0";
 
 	/**
 	 * Data version for NetStatusEvents
