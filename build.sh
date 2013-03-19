2271062f2c2cc8337b19260275530d429b50e32d
Richard Alam <ritzalam@gmail.com>
2013-03-19 21:49:14 +0000

    - store jar file we need for desktop sharing

--- build.sh
+++ build.sh
@@ -11,21 +11,22 @@ mvn -Dmaven.test.skip=true -Dclassifier=bootstrap package
 cd target
 echo "Extractring red5"
 tar zxvf red5-server-1.0.2-SNAPSHOT-server.tar.gz
-cd red5-server-1.0.2-SNAPSHOT
 #mv red5-server-1.0.2-SNAPSHOT-bootstrap.jar red5-server-1.0-bootstrap.jar
 #mv red5-server-1.0.2-SNAPSHOT.jar red5-server-1.0.jar
-rm lib/red5-client-*.jar
+rm red5-server-1.0.2-SNAPSHOT/lib/red5-client-*.jar
 
 echo "Deploying red5"
-cd /home/firstuser/dev/red5/red5/target
+cd ..
 sudo rm -rf /usr/share/red5
-sudo rm -rf /usr/share/red5-r4597
-sudo cp -R red5-server-1.0.2-SNAPSHOT /usr/share/red5-r4597
-sudo ln -s /usr/share/red5-r4597 /usr/share/red5
-sudo cp /usr/share/red5-r4416/lib/commons-fileupload-1.2.2.jar /usr/share/red5/lib/
+sudo rm -rf /usr/share/red5-r4599
+sudo cp -R target/red5-server-1.0.2-SNAPSHOT /usr/share/red5-r4599
+sudo ln -s /usr/share/red5-r4599 /usr/share/red5
+sudo cp extras/commons-fileupload-1.2.2.jar /usr/share/red5/lib/
+sudo cp extras/red5.sh /usr/share/red5
+sudo chmod 755 /usr/share/red5/red5.sh
 sudo chown -R red5.adm /usr/share/red5
-sudo chown -R red5.adm /usr/share/red5-r4597
-sudo chmod -R 777 /usr/share/red5-r4597/webapps/
+sudo chown -R red5.adm /usr/share/red5-r4599
+sudo chmod -R 777 /usr/share/red5-r4599/webapps/
 
 echo "Red5 ready"
 
